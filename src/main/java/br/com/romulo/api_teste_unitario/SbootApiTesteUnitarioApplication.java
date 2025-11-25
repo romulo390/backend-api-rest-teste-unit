efe746928fcc7b51f6c4eeb820f00cd46ad45407
romulo390 <cromuloaraujo@gmail.com>
2025-11-25 15:04:14 -0300

    Primeiro commit

---
+++ src/main/java/br/com/romulo/api_teste_unitario/SbootApiTesteUnitarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.romulo.api_teste_unitario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootApiTesteUnitarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootApiTesteUnitarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/romulo/api_teste_unitario/SbootApiTesteUnitarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.romulo.api_teste_unitario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootApiTesteUnitarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
