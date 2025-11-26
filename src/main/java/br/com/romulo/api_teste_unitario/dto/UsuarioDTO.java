bf78f27f97006055dd6368f61857295ed866661f
romulo390 <cromuloaraujo@gmail.com>
2025-11-26 15:58:16 -0300

    incluindo dependencia modelmapper

---
+++ src/main/java/br/com/romulo/api_teste_unitario/dto/UsuarioDTO.java
@@ -0,0 +1,17 @@
+package br.com.romulo.api_teste_unitario.dto;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioDTO {
+
+    private Integer idUsuario;
+    private String nome;
+    private String email;
+    private String senha;
+}
